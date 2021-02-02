592138ebe3ef974d91aeef72cbf0081b87bb2150
hasmukh96 <77569059+hasmukh96@users.noreply.github.com>
2021-02-02 11:01:53 +0530

    Create basics_1.py

---
+++ basics_1.py
@@ -0,0 +1,20 @@
+Data type:
+1. String: string data type is used for storing alpabets
+2. Numeric: data type for numbers.
+  int - used for stroing whole numbers
+  float - used for numbers where decimals are there.
+  complex - used for storing complex number (a+bj)
+3. Sequence: used for data where grouping is req.
+  List - used where list of item is needed to stored. it is changable
+  tuple - used like list but the items are unchangable 
+  set - used like list but only unique values are stored.
+4. Boolean: used for True (1) and False (0)
+5. Binary: uses 0 and 1 to define any number 
+  Oct - has a range of 0-7
+  Hex - has a range of 16 i.e. 0-7 & a-f
+ Bytes: it is a data type which helps to convert data type to binary
+        only contain value of range of 0-255
+        stores in hexadecimal format
+ Bytearray: changable 
+ Memoryview: it is useful for looking into the data of object (for eg bytearray)
+ it can also help to change the values of the object
