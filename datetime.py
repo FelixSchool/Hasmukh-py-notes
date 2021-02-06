3c848122fcdd6df3cf669769448a86f4e777dc3d
Hasmukh Suthar <hfsuthar96@gmail.com>
2021-02-06 21:09:07 +0530

    commit

---
+++ datetime.py
@@ -0,0 +1,10 @@
+import datetime
+dob = datetime.date(1996,5,23)
+print(dob.strftime("%B %drd %Y"))
+print(dob.strftime("%drd %B %Y"))
+print(dob.strftime("%B %d,%Y"))
+print(dob.strftime("%b-%d-%Y"))
+print(dob.strftime("%m-%d-%y"))
+print(dob.strftime("%m-%d-%Y"))
+print(dob.strftime("%d/%m/%Y"))
+print(dob.strftime("%B the %drd, the year %Y"))
